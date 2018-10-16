bda54e04109c7e929f3954995c9a48e7ec3bc2a3
Miguel <mgck1@aol.com>
2018-10-16 11:21:38 -0500

    Angular Material install and Angular component build tool.

---
+++ EvaluationPerformanceFormsAspCore.Web/ClientApp/src/app/executive-form/executive-form.component.spec.ts
@@ -0,0 +1,25 @@
+﻿/// <reference path="../../../../node_modules/@types/jasmine/index.d.ts" />
+import { TestBed, async, ComponentFixture, ComponentFixtureAutoDetect } from '@angular/core/testing';
+import { BrowserModule, By } from "@angular/platform-browser";
+import { ExecutiveFormComponent } from './executive-form.component';
+
+let component: ExecutiveFormComponent;
+let fixture: ComponentFixture<ExecutiveFormComponent>;
+
+describe('executive-form component', () => {
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ ExecutiveFormComponent ],
+            imports: [ BrowserModule ],
+            providers: [
+                { provide: ComponentFixtureAutoDetect, useValue: true }
+            ]
+        });
+        fixture = TestBed.createComponent(ExecutiveFormComponent);
+        component = fixture.componentInstance;
+    }));
+
+    it('should do something', async(() => {
+        expect(true).toEqual(true);
+    }));
+});
---
+++ EvaluationPerformanceFormsAspCore.Web/ClientApp/src/app/executive-form/executive-form.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-executive-form',
+    templateUrl: './executive-form.component.html',
+    styleUrls: ['./executive-form.component.scss']
+})
+/** executive-form component*/
+export class ExecutiveFormComponent {
+    /** executive-form ctor */
+    constructor() {
+
+    }
+}
